c5b43d350084658820df304c2e7a0f5b019d3fe3
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-11 19:50:18 +0100

    Update A.cpp

--- 101/A.cpp
+++ 101/A.cpp
@@ -1,44 +1,6 @@
-
 #include <iostream>
 #include <string>
 #include <cmath>
-#include <stdio.h>
-    //----------------------------------------------
-    /*int x,z,c=0,f=0;
-    cin>>x;
-    int *y=new int [x];
-    z=x;
-    while(z-->0) cin>>y[f++];
-    z=x;
-    f=0;
-    while(z-->1)
-    {
-        if(y[f]<y[f+++1]) continue;
-        else {c=-1; break;}
-    }
-    if(c==0) cout<<"Ordered\n0\n";
-    else{
-        c=0;
-        for(int i=0;i<x-1;i++)
-            for(int j=0;j<(x-1)-i;j++)
-                if(y[j]>y[j+1]){swap(y[j+1],y[j]); c++;}
-        f=1;
-        cout<<y[0];
-        while(x-->1) cout<<" "<<y[f++];
-        cout<<endl<<c<<endl;
-    }*/
-    //--------------------------------------------
-    /*int x=0,z=0,m=0,c=0;
-    cin>>x;
-    z=x;
-    char *y=new char[x];
-    while(z-->0&&++m)
-    {
-        int f=0,s=x;
-        while(s-->0) {cin>>y[f++]; if(y[f-1]=='#') {cout<<m<<" "<<f<<endl; c++;}}
-    }
-    if(c==0) cout<<"No hiding places"<<endl;
-    else cout<<c<<endl;*/
     //-----------------------------------------------
     /*unsigned long long z,x,y,c,m,s;
     cin>>x;
