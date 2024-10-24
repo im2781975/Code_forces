6f9906ce1f60dd7eb1914a96d4030d8e8d4bf224
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-10-24 20:30:29 +0200

    Update .cpp

--- 101/.cpp
+++ 101/.cpp
@@ -14,3 +14,19 @@ int main(){
         ans += func(i);
     cout << ans;
 }
+using namespace std;
+int GCD(int a, int b){
+    if(a % b == 0)
+        return b;
+    return GCD(b, a % b);
+}
+int main(){
+    int ans = 0;
+    for(int i = 1; i <= 2020; i++){
+        for(int j = 1; j <= 2020; j++){
+            if(GCD(i, j) == 1)
+                ans++;
+        }
+    }
+    cout << ans;
+}
