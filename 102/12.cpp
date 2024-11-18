57b31eb6462aa9426e308b1d49dd9beb966e4aad
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-11-18 17:34:05 +0100

    Update 12.cpp

--- 102/12.cpp
+++ 102/12.cpp
@@ -908,3 +908,652 @@ int main()
      solve();
 }
 //https://codeforces.com/problemset/problem/195/A
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main(int argc, char const *argv[])
+// {
+//     char arr[100];
+//     char app[100];
+//     cin >> arr;
+//     int t = 0, m = 0;
+//     for (int i = 0; arr[i] != '\0'; i++)
+//     {
+//         t++;
+//     }
+
+//     cout << t << endl;
+//     for (int i = 0; i < t; i++)
+//     {
+//         if (arr[i] == arr[t - 1 - i])
+//         {
+//             m++;
+//             continue;
+//         }
+//     }
+
+//     cout << m << endl;
+//     if (t == m)
+//     {
+//         cout << "its a palindrone number ";
+//     }
+//     else
+//     {
+//         cout << "its a not a palindrone number";
+//     }
+
+//     return 0;
+// }
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main(int argc, char const *argv[])
+// {
+//     string str;
+//     getline(cin, str);
+//     int t = 0, m = 0, q;
+//     int p = str.size();
+//     cout << p << endl;
+//     for (int i = 0; i < str.size(); i++)
+//     {
+//         if (str[i] == ' ')
+//         {
+//             t = m;
+//             m = 0;
+//         }
+//         if (str[i] != ' ')
+//         {
+//             m++;
+//             q = max(m, t);
+//         }
+//     }
+
+//     cout << q;
+
+//     return 0;
+// }
+// #include <bits/stdc++.h>
+// using namespace std;
+// int search(int *a, int p);
+// int m = 0;
+
+// int main(int argc, char const *argv[])
+// {
+//     int a[5] = {4, 3, 3, 3, 8};
+//     int p;
+//     cin >> p;
+//     for (int i = 0; i < 5; i++)
+//     {
+
+//         if (p == a[i])
+//         {
+//             cout<<i<<endl;
+//             search(a, p);
+//             break;
+//         }
+//     }
+
+//     return 0;
+// }
+// int search(int *a, int p)
+// {
+//     if (a[5 - m] == p)
+//     {
+//         cout << (5 - m);
+//         return 0;
+//     }
+// m++;
+// return (search(a, p));
+// }
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main(int argc, char const *argv[])
+// {
+//     int n;
+//     cin >> n;
+//     string str[n];
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> str[i];
+//     }
+//     for (int i = 0; i < n; i++)
+//     {
+//         if (str[i].size() <= 10)
+//         {
+//             cout << str[i] << endl;
+//         }
+//         if (str[i].size() > 10)
+//         {
+//             int p = str[i].size() - 2;
+
+//             str[i].erase(1, p);
+//             string d = to_string(p);
+//             str[i].insert(1, d);
+
+//             cout << str[i] << endl;
+//         }
+//     }
+//     return 0;
+// }
+// nclude<bits/stdc++.h>
+// ing namespace std;
+// t main(int argc, char const *argv[])
+// {
+// string str;
+// cin>>str;
+// str.erase(1,4);
+// cout<<str;
+// return 0;
+// }
+//
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main(int argc, char const *argv[])
+// {
+//     int n;
+//     cin >> n;
+//     int a[n][3];
+//     int r = 0, m = 0;
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int j = 0; j < 3; j++)
+//         {
+//             cin >> a[i][j];
+//         }
+//     }
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int j = 0; j < 3; j++)
+//         {
+//             r = a[i][j] + r;
+//             if (r >= 2)
+//             {
+//                 m++;
+//                 r = 0;
+//                 break;
+//             }
+//         }
+//         r = 0;
+//     }
+//     cout << m;
+
+//     return 0;
+// }
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main(int argc, char const *argv[])
+// {
+//     int n, k;
+//     cin >> n >> k;
+//     int a[n];
+//     int p = 0;
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> a[i];
+//     }
+//     for (int i = 1; i <= n; i++)
+//     {
+//         if (a[i] >= i)
+//         {
+//             p++;
+//         }
+//     }
+//     cout << p
+//  return 0;
+//  }
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int r = 0;
+//     int n;
+//     cin >> n;
+//     string str[n];
+//     for (int i = 0; i < n; i++)
+//     {
+
+//         cin >> str[i];
+//     }
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         if (str[i] == "++X")
+//         {
+//             ++r;
+//         }
+//         if (str[i] == "X++")
+//         {
+//             r++;
+//         }
+//         if (str[i] == "--X")
+//         {
+
+//             --r;
+//         }
+//         if (str[i] == "X--")
+//         {
+//             r--;
+//         }
+//     }
+
+//     cout << r;
+//     return 0;
+// }
+
+//  #include <bits/stdc++.h>
+//  using namespace std;
+// int main(int argc, char const *argv[])
+// {
+//     int a[5][5];
+//     int p, q;
+//     int r = 0;
+//     for (int i = 0; i < 5; i++)
+//     {
+//         for (int j = 0; j < 5; j++)
+//         {
+//             cin >> a[i][j];
+//         }
+//     }
+
+//     for (int i = 0; i < 5; i++)
+//     {
+//         for (int j = 0; j < 5; j++)
+//         {
+//             if (1 == a[i][j])
+//             {
+//                 p = i;
+
+//                 q = j;
+
+//                 break;
+//             }
+//         }
+//     }
+//     while (p != 2)
+//     {
+
+//         if (p > 2)
+//         {
+//             int t;
+//             t = a[p][q];
+//             a[p][q] = a[p - 1][q];
+//             a[p - 1][q] = t;
+//             p = p - 1;
+//             ++r;
+//         }
+//         if (p < 2)
+//         {
+//             int t;
+//             t = a[p][q];
+//             a[p][q] = a[p + 1][q];
+//             a[p + 1][q] = t;
+//             p = p + 1;
+//             ++r;
+//         }
+//     }
+//     while (q != 2)
+//     {
+//         if (q > 2)
+//         {
+//             int t;
+//             t = a[p][q];
+//             a[p][q] = a[p][q - 1];
+//             a[p][q - 1] = t;
+//             q = q - 1;
+//             ++r;
+//         }
+//         if (q < 2)
+//         {
+//             int t;
+//             t = a[p][q];
+//             a[p][q] = a[p][q + 1];
+//             a[p][q + 1] = t;
+//             q = q + 1;
+//             ++r;
+//         }
+//     }
+
+//     cout << r;
+//     return 0;
+// }
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main(int argc, char const *argv[])
+// {
+//     int n, k;
+//     cin >> n >> k;
+//     int a[n];
+//     int p = 0;
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> a[i];
+//     }
+//     for (int i = 1; i <= n; i++)
+//     {
+//         if (a[i] >= i)
+//         {
+//             p++;
+//         }
+//     }
+//     cout << p;
+
+//     return 0;
+// }
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main(int argc, char const *argv[])
+// {
+//     long long n, m, a;
+//     cin >> n >> m >> a;
+//     long long p = 0, q = 0;
+//     for (long i = 1; i <= n; i = i + a)
+//     {
+//         p++;
+//     }
+//     for (long j = 1; j <= m; j = j + a)
+//     {
+//         q++;
+//     }
+//     cout<<p*q;
+
+//     return 0;
+// }
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main(int argc, char const *argv[])
+// {
+//     int m, n;
+//     cin >> m >> n;
+//     long long p = m * n;
+//     int r = 2;
+//     long long g = p / r;
+
+//     cout << g;
+
+//     return 0;
+// }
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main(int argc, char const *argv[])
+// {
+//     string str1, str2;
+//     getline(cin, str1);
+//     getline(cin, str2);
+//     int i = 0, u = 0;
+//     int p = 0, q = 0;
+//     for (; i < str1.size(); i++)
+//     {
+//         if (str1[i] == str2[i] || str1[i] == str2[i] + 32 || str2[i] == str1[i] + 32)
+//         {
+//             u++;
+//             continue;
+//         }
+
+//         if (str1[i] > str2[i])
+//         {
+//             p = p + str1[i];
+//             q = q + str2[i];
+//         }
+//         if (str1[i] < str2[i])
+//         {
+//             p = p + str1[i];
+//             q = q + str2[i];
+//         }
+//     }
+//     if (u == str1.size())
+//     {
+//         cout << 0;
+//     }
+//     if (p > q)
+//     {
+//         cout << 1;
+//     }
+//     if (q > p)
+//     {
+//         cout << -1;
+//     }
+
+//     return 0;
+// }
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main(int argc, char const *argv[])
+// {
+//     string str;
+//     getline(cin, str);
+//     int p = 0;
+//     for (int i = 0; i < 8; i++)
+//     {
+//         for (int i = 0; i < str.size(); i++)
+//         {
+
+//             if (str[i] != 'A' && str[i] != 'E' && str[i] != 'I' && str[i] != 'O' && str[i] != 'U' && str[i] != 'y')
+//             {
+//                 if (str[i] >= 65 && str[i] <= 91)
+//                 {
+//                     str[i] = str[i] + 32;
+//                 }
+//             }
+//             if (str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U' || str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u'|| str[i] == 'y')
+//             {
+//                 str.erase(i, 1);
+//             }
+//         }
+//     }
+
+//     for (int i = 0; i < str.size(); i = i + 2)
+//     {
+//         str.insert(i, ".");
+//     }
+
+//     for (int i = 0; i < str.size(); i++)
+//     {
+//         cout << str[i];
+//     }
+
+//     return 0;
+// }
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main(int argc, char const *argv[])
+// {
+//     string str;
+//     getline(cin, str);
+
+//     for (int i = 0; i < str.size(); i = i + 1)
+//     {
+//         for (int j = 0; j < str.size() - 1; j = j + 2)
+//         {
+//             if (str[j] > str[j + 2])
+//             {
+//                 char c;
+//                 c = str[j];
+//                 str[j] = str[j + 2];
+//                 str[j + 2] = c;
+//             }
+//         }
+//     }
+//     cout << str << endl;
+//     return 0;
+// }
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main(int argc, char const *argv[])
+// {
+//     string str;
+//     getline(cin, str);
+//     if (str[0] >= 97 && str[0] <= 123)
+//     {
+//         str[0] = str[0] - 32;
+//     }
+//     cout<<str;
+//     return 0;
+// }
+// Input
+// zcinitufxoldnokacdvtmdohsfdjepyfioyvclhmujiqwvmudbfjzxjfqqxjmoiyxrfsbvseawwoyynn
+// Participant's output
+// CHAT WITH HER!
+// Jury's answer
+// IGNORE HIM!
+// Checker comment
+// wrong answer 1st lines differ - expected: 'IGNORE HIM!', found: 'CHAT WITH HER!'
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main(int argc, char const *argv[])
+// {
+//     string str;
+//     string str1;
+
+//     getline(cin, str);
+
+//     for (int r = 0; r < 4; r++)
+//     {
+//         for (int i = 0; i < str.size(); i++)
+//         {
+//             for (int j = i + 1; j < str.size(); j++)
+//             {
+//                 if (str[i] == str[j])
+//                 {
+//                     str.erase(j, 1);
+//                 }
+//             }
+//         }
+//     }
+
+//     int p = str.size();
+
+//     if (p % 2 == 1)
+//     {
+//         cout << "IGNORE HIM!";
+//     }
+//     else
+//     {
+//         cout << "CHAT WITH HER!";
+//     }
+
+//     return 0;
+// }
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main(int argc, char const *argv[])
+// {
+//     int n, z;
+//     cin >> n;
+//     int a[n], b[100001], d[100001];
+//     long long r = 0, t = 0;
+//     int taxi = 0;
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> a[i];
+//     }
+
+//     for (int j = 0; j < n; j++)
+//     {
+//         if (a[j] % 2 == 1)
+//         {
+//             b[r] = a[j];
+//             r++;
+//         }
+
+//         if (a[j] % 2 == 0)
+//         {
+//             if (a[j] % 4 == 0)
+//             {
+//                 taxi++;
+//             }
+//             if (a[j] % 4 != 0)
+//             {
+//                 d[t] = a[j];
+//                 t++;
+//             }
+//         }
+//     }
+
+//     // for (int i = 0; i < r; i++)
+//     // {
+//     //     cout << b[i] << endl;
+//     // }
+//     // for (int i = 0; i < t; i++)
+//     // {
+//     //     cout << d[i] << endl;
+//     // }
+
+//     // cout << taxi << endl;
+//     //  for (int i = 0; i < r; i++)
+//     //  {
+//     //  cout << b[i] << end
+
+//     long long m = 0, u = 0, y = 0;
+//     for (int i = 0; i < r; i++)
+//     {
+//         m = m + b[i];
+//     }
+//     for (int i = 0; i < t; i++)
+//     {
+//         u = u + d[i];
+//     }
+//     for (int i = 0; i < r; i++)
+//     {
+//         if (b[i] == 3)
+//         {
+//             y++;
+//         }
+//     }
+//     if (y == r)
+//     {
+//         cout << r;
+//         goto z;
+//     }
+
+//     r = m / 4;
+
+//     taxi = taxi + r;
+
+//     r = m % 4;
+
+//     t = u / 4;
+
+//     taxi = taxi + t;
+
+//     t = u % 4;
+
+//     if ((t + r) <= 4 && (t + r) > 0)
+//     {
+//         taxi = taxi + 1;
+//     }
+//     if (t == r && (t + r) == 4 && m == 6 && u == 2)
+//     {
+//         taxi = taxi + 1;
+//     }
+
+//     if ((t + r) > 4)
+//     {
+//         taxi = taxi + 2;
+//     }
+
+//     cout << taxi;
+// z:
+//     return 0;
+// }
+#include <bits/stdc++.h>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    int n;
+    cin >> n;
+
+    char c[20];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> c[i];
+    }
+    int t = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (c[i] == c[i + 1])
+        {
+            t++;
+        }
+    }
+    cout << t;
+
+    return 0;
+}
