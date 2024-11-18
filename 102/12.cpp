812f6831ab74c7b4c902d75e20c2bb431e7a63af
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-11-18 20:01:01 +0100

    Update 12.cpp

--- 102/12.cpp
+++ 102/12.cpp
@@ -125,87 +125,17 @@ long BinPow(int a, int b){
     }
     return res;
 }
-#include <bits/stdc++.h>
-using namespace std;
-#define ll long long int
-#define fo(i, p, n) for (ll i = p; i < n; i++)
-const ll MAXN = 1;
-const ll M = 998244353;
-
-// long long binpow1(long long a, long long b, long long m) {
-//     a %= m;
-//     long long res = 1;
-//     while (b > 0) {
-//         if (b & 1)
-//             res = res * a % m;
-//         a = a * a % m;
-//         b >>= 1;
-//     }
-//     return res;
-// }
-long long binpow(long long a, long long b)
-{
-
-	long long res = 1;
-	while (b > 0)
-	{
-		if (b & 1)
-			res = res * a;
-		a = a * a;
-		b >>= 1;
-	}
-	return res;
+long long nCr(int n, int p, int r){
+    if(n < r)
+        return 0;
+    if(r == 0)
+        return 1;
+    int fact[n + 1];
+    fact[0] = 1;
+    for(int i = 1; i <= n; i++)
+        fact[i] = (fact[i - 1] * i) % p;
+    return (fact[n] * BinPow(fact[r], p - 2, p) % p * BinPow(fact[n - r], p - 2, p) % p) % p;
 }
-
-unsigned long long power(unsigned long long x,
-						 ll y, ll p)
-{
-	unsigned long long res = 1; // Initialize result
-
-	x = x % p; // Update x if it is more than or
-	// equal to p
-
-	while (y > 0)
-	{
-
-		// If y is odd, multiply x with result
-		if (y & 1)
-			res = (res * x) % p;
-
-		// y must be even now
-		y = y >> 1; // y = y/2
-		x = (x * x) % p;
-	}
-	return res % M;
-}
-unsigned long long modInverse(unsigned long long n,
-							  ll p)
-{
-	return power(n, p - 2, p);
-}
-
-unsigned long long nCrModPFermat(unsigned long long n,
-								 ll r, ll p)
-{
-	// If n<r, then nCr should return 0
-	if (n < r)
-		return 0;
-	// Base case
-	if (r == 0)
-		return 1;
-
-	// Fill factorial array so that we
-	// can find all factorial of r, n
-	// and n-r
-	unsigned long long fac[n + 1];
-	fac[0] = 1;
-	for (ll i = 1; i <= n; i++)
-		fac[i] = (fac[i - 1] * i) % p;
-
-	return (fac[n] * modInverse(fac[r], p) % p * modInverse(fac[n - r], p) % p) % p;
-}
-
-
 // bool prime[1000100];
 // void isprime()
 // {
