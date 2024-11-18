d58ad8a0e4e59379f4de753b4d2c07c6f322226f
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-11-18 19:25:44 +0100

    Update 12.cpp

--- 102/12.cpp
+++ 102/12.cpp
@@ -101,6 +101,30 @@ int main(){
     }
     cout << ForwardCnt << " " << BackwardCnt;
 }
+using namespace std;
+long BinPow(int a, int b, int m){
+    a %= m;
+    if(b == 0)
+        return 1;
+    long long res = 1;
+    while(b > 0){
+        if(b & 1)
+            res = res * a % m;
+        a = a * a % m;
+        b >> = 1;
+    }
+    return res;
+}
+long BinPow(int a, int b){
+    long long res = 1;
+    while(b > 0){
+        if(b & 1)
+            res *= a;
+        a *= a;
+        b >> = 1;
+    }
+    return res;
+}
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long int
