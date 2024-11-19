e7e1a8f8dcef9735ed6b622f8a63168f994ec7da
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-11-19 23:13:08 +0100

    Update 1 .cpp

--- 102/1 .cpp
+++ 102/1 .cpp
@@ -1,4 +1,16 @@
 using namespace std;
+void Megnets(){
+    int n, cnt = 0;
+    int arr[n];
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+    for(int i = 0; i < n - 1; i++){
+        if(arr[i] != arr[i - 1])
+            cnt++;
+    }
+    cout << cnt;
+}
+using namespace std;
 void BitsPlus(){
     int n, cnt = 0;
     cin >> n;
