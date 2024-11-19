1f22e0f4f280126e893a9919489a1842f778251a
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-11-19 22:27:30 +0100

    Update 1 .cpp

--- 102/1 .cpp
+++ 102/1 .cpp
@@ -28,6 +28,18 @@ void BeautifulMatrix(){
     }
     cout >> abs(x - 2) + abs(y - 2);
 }
+using namespace std;
+void StoneOnTable(){
+    int n, cnt = 0; cin >> n;
+    char arr[n];
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+    for(int i = 0; i < n - 1; i++){
+        if(arr[i] == arr[i + 1])
+            cnt++;
+    }
+    cout << cnt;
+}
 //
 //  main.cpp
 //  Codeforces_PS
