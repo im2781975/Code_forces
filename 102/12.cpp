02055c983bc13c1e06b967aa8cb5e36551bb23e9
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-11-19 11:59:32 +0100

    Update 12.cpp

--- 102/12.cpp
+++ 102/12.cpp
@@ -201,54 +201,18 @@ int cntDisjoint(const vector <pair <int, int> > &vec){
     }
     return cnt;
 }
-// vector< pair<ll, ll> > findSubArrays(ll arr[], ll n)
-// {
-//     // create an empty map
-//     unordered_map<ll, vector<ll> > map;
-
-//     // create an empty vector of pairs to store
-//     // subarray starting and ending index
-//     vector <pair<ll, ll>> out;
-
-//     // Maintains sum of elements so far
-//     ll sum = 0;
-
-//     for (ll i = 0; i < n; i++)
-//     {
-//         // add current element to sum
-//         sum += arr[i];
-
-//         // if sum is 0, we found a subarray starting
-//         // from index 0 and ending at index i
-//         if (sum == 0)
-//             out.push_back(make_pair(0, i));
-
-//         // If sum already exists in the map there exists
-//         // at-least one subarray ending at index i with
-//         // 0 sum
-//         if (map.find(sum) != map.end())
-//         {
-//             // map[sum] stores starting index of all subarrays
-//             vector<ll> vc = map[sum];
-//             for (auto it = vc.begin(); it != vc.end(); it++)
-//                 out.push_back(make_pair(*it + 1, i));
-//         }
-
-//         // Important - no else
-//         map[sum].push_back(i);
-//     }
-
-//     // return output vector
-//     return out;
-// }
-
-// // Utility function to print all subarrays with sum 0
-// // ll print(vector<pair<ll, ll>> out)
-// // {ll dp=1; ll pr=out.begin()->second;
-// //     for (auto it = out.begin()+1; it != out.end(); it++)
-// //         if(it->second>pr){dp++; pr=it->second;}
-// //         return dp;
-// // }
+using namespace std;
+int longestSubseq(vector <int> vec){
+    vector <int> ans;
+    for(int i = 0; i < vec.size(); i++){
+        auto it = upper_bound(ans.begin(), ans.end(), vec[i]);
+        if(it == ans.end())
+            ans.push_back(vec[i]);
+        else
+            *it = vec[i];
+    }
+    return ans.size();
+}
 // #define MAXN   (ll)1e7+1
 
 // // stores smallest prime factor for every number
