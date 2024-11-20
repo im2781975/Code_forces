cf1856738d04edefab20097c8b47137e828344ee
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-11-20 11:21:27 +0100

    Update 1 .cpp

--- 102/1 .cpp
+++ 102/1 .cpp
@@ -128,6 +128,18 @@ int main(){
     }
     cout << cntMax + cntMin;
 }
+using namespace std;
+int main(){
+    int n, cnt = 0; cin >> n;
+    int arr[n];
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+    for(int i = 0; i < n; i++){
+        if(arr[i] == 1)
+            cnt++;
+    }
+    (cnt == 0) ? cout << "Easy" : cout << "Hard";
+}
 #include <iostream>
 
 using namespace::std;
