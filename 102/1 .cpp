847f1bbc3835c059c779b8bca735aaba05268648
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-11-23 21:50:10 +0100

    Update 1 .cpp

--- 102/1 .cpp
+++ 102/1 .cpp
@@ -1,4 +1,16 @@
 using namespace std;
+void HorseShoweOnHoof(){
+    int arr[4], cnt = 0;
+    for(int i = 0; i < 4; i++)
+        cin >> arr[i];
+    sort(arr, arr + 4);
+    for(int i = 0; i < 3; i++){
+        if(arr[i] == arr[i + 1])
+            cnt++;
+    }
+    cout << cnt;
+}
+using namespace std;
 void Juicer(){
     int n, b, d;cin >> n >> b >> d;
     vector <int> vec(n);
