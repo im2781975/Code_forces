0b580b04be90a72c0abed626bf300748e775090c
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-11-25 22:48:48 +0100

    Update 1 .cpp

--- 102/1 .cpp
+++ 102/1 .cpp
@@ -1,3 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct TrieNode {
+    unordered_map<char, TrieNode*> m;
+    char c;
+    bool isWord;
+    TrieNode() : c(0), isWord(false) {}
+    TrieNode(char ch) : c(ch), isWord(false) {}
+};
+class Trie {
+    TrieNode* root;
+    public:
+    Trie() {
+        root = new TrieNode();
+    }
+    ~Trie() {
+        deleteTrie(root);
+    }
+    void insert(const string& word) {
+        TrieNode* p = root;
+        for (char c : word) {
+            if (p->m.find(c) == p->m.end()) {
+                p->m[c] = new TrieNode(c);
+            }
+            p = p->m[c];
+        }
+        p->isWord = true;
+    }
+    bool search(const string& word) const {
+        TrieNode* p = root;
+        for (char c : word) {
+            if (p->m.find(c) == p->m.end()) return false;
+            p = p->m[c];
+        }
+        return p->isWord;
+    }
+    bool startsWith(const string& prefix) const {
+        TrieNode* p = root;
+        for (char c : prefix) {
+            if (p->m.find(c) == p->m.end()) return false;
+            p = p->m[c];
+        }
+        return true;
+    }
+    private:
+    void deleteTrie(TrieNode* node) {
+        for (auto& pair : node->m)
+            deleteTrie(pair.second);
+        delete node;
+    }
+};
+
+
 class Solution { 
 public: 
     int minimumCost(vector<int>& v) { 
