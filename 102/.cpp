374183bebc6aba9d25eeac89762a606526bab31c
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-11-25 07:40:59 +0100

    Update .cpp

--- 102/.cpp
+++ 102/.cpp
@@ -69,3 +69,69 @@ void task768_2_A()(){
     //maxB = *max_element(vec.begin(), vec.end());
     cout << maxA * maxB;
 }
+using namespace std;
+void task122_Rated_2_C() {
+    ll heroHealth, heroDamage, monsterHealth, monsterDamage, upgrades, damagePerUpgrade, healthPerUpgrade;
+    cin >> heroHealth >> heroDamage >> monsterHealth >> monsterDamage >> upgrades >> damagePerUpgrade >> healthPerUpgrade;
+    for (ll currentHealthUpgrades = 0; currentHealthUpgrades <= upgrades; ++currentHealthUpgrades) {
+        ll upgradedHealth = heroHealth + healthPerUpgrade * currentHealthUpgrades;
+        ll upgradedDamage = heroDamage + damagePerUpgrade * (upgrades - currentHealthUpgrades);
+        ll roundsToKillMonster = (monsterHealth + upgradedDamage - 1) / upgradedDamage;
+        ll roundsToSurvive = (upgradedHealth + monsterDamage - 1) / monsterDamage;
+        if (roundsToKillMonster <= roundsToSurvive) {
+            cout << "YES";
+            return;
+        }
+    }
+    cout << "NO";
+}
+using namespace std;
+void task122_Rated_2_B()(){
+    string str; cin >> str;
+    int zero = 0, one = 0, len = str.size();
+    for(int i = 0; i < len; i++){
+        if(str[i] == '0')
+            zero++;
+        else
+            one++;
+    }
+    if(zero != one)
+        cout << min(zero, one);
+    else{
+        int first, sec;
+        if(str[len - 1] == '0')
+            first = min(zero - 1, one);
+        else
+            first = min(zero, one - 1);
+        if(str[0] == '0')
+            sec = min(zero - 1, one);
+        else
+            sec = min(zero, one - 1);
+        cout << max(first, sec);
+    }
+}
+using namespace std;
+void task767_2_B() {
+    int l, r, k; cin >> l >> r >> k;
+    if (l == r) {
+        if (l == 1)
+            cout << "NO";
+        else
+            cout << "YES";
+        return;
+    }
+    if ((r - l + 1) - (r / 2 - (l - 1) / 2) <= k)
+        cout << "YES";
+    else
+        cout << "NO";
+}
+using namespace std;
+void task122_Rated_2_A() {
+    int n; cin >> n;
+    if (n % 7 == 0)
+        cout << n;
+    else if (n % 10 > n % 7)
+        cout << n - n % 7;
+    else
+        cout << n + 7 - n % 7;
+}
