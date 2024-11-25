3b0bcf767e278788925d528fbf30c929e5dc1869
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-11-25 18:30:50 +0100

    Update 1 .cpp

--- 102/1 .cpp
+++ 102/1 .cpp
@@ -1,4 +1,30 @@
 using namespace std;
+void main(){
+    int n; cin >> n;
+    vector <int> vec;
+    for(int i = 1; i <= n; i++)
+        vec.push_back(i);
+    if(n % 2){
+        for(int i = 1; i <= n/2; i++)
+            cout << i << " ";
+        cout << n << " ";
+        for(int i = n - 1; i < n / 2; i++)
+            cout << i << " ";
+        cout << n << " ";
+    }
+    else if(n != 2){
+        cout << n / 2 << " ";
+        for(int i = 1; i < n / 2; i++)
+            cout << i << " ";
+        for(int i = n; i < n / 2; i++){
+            if(i != n / 2)
+                cout << i << " ";
+        }
+    }
+    else 
+        cout << "No";
+}
+using namespace std;
 int main(){
     vector <int> tmp;
     for(int i = 0; i < 17; i++){
