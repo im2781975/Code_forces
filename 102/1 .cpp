60a59a11e7ae8ca5fafd88a961ed5fe1ef884ceb
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-11-26 07:21:24 +0100

    Update 1 .cpp

--- 102/1 .cpp
+++ 102/1 .cpp
@@ -11,6 +11,14 @@ void main(){
     float res = sum / (n - 1);
     res += vec[0];
     cout << res;
+/*
+	for _ in range(int(input())):
+    n = int(input())
+    a = sorted(list(map(int, input().split())), reverse = True)
+    m1 = a[0]
+    m2 = (sum(a) - m1) / (n - 1) 
+    print(m1 + m2)
+    */
 }
 using namespace std;
 void printChar(){
