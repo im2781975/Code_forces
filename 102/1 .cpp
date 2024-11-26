6aaafb0d3dd0019923b318a8a540b98398cfd379
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-11-26 08:30:50 +0100

    Update 1 .cpp

--- 102/1 .cpp
+++ 102/1 .cpp
@@ -1,3 +1,17 @@
+def sieve(n):
+    prime = [True for _ in range(n + 1)]
+    p = 2
+    while p * p <= n:
+        if prime[p]:
+            for i in range(p * p, n + 1, p):
+                prime[i] = False
+        p += 1
+    primes = []
+    for p in range(2, n + 1):
+        if prime[p]:
+            primes.append(p)
+    return primes
+primes = sieve(10**5)
 using namespace std;
 void main(){
     int n; cin >> n;
