13919b5a57308a05ac63814bac503347a22230fe
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-11-27 05:21:02 +0100

    Update 1 .cpp

--- 102/1 .cpp
+++ 102/1 .cpp
@@ -1,5 +1,25 @@
 using namespace std;
 void main(){
+    int n; cin >> n;
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+        if(arr[i] % n == 0)
+            cout << "1";
+        else
+            cout << "0";
+    }
+    for(int i = 0; i < n; i++){
+        for(int j = i; j < n; j++){
+            int sum = 0;
+            for(int k = i; k <= j; k++)
+                sum += arr[k];
+            cout << "sum from idx " << i << " to " << j << " is " << sum << "\n";
+        }
+    }
+}
+using namespace std;
+void main(){
     int n, m; cin >> n >> m;
     vector <int> vec(n + 1);
     for(int i = 1; i <= n; i++)
